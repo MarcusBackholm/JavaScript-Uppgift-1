aa1f51b056dc00cd2c695f83f0606448bef2c158
Marcus Backholm <marcus.backholm@live.se>
2021-02-01 23:35:10 +0100

    Fixat JS kod och kommenterat hur den fungerar till stor del samt fixat det estetiska för css i HTML dock funkar inte css för inläggen då rutorna sitter ihop.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
-let inputCounter = document.getElementById("input-number");
-inputCounter.addEventListener('change', setEqual);
+document.addEventListener("DOMContentLoaded", onPageLoad);
 
-function setEqual()
-{
-    let smallTextInput = document.createElement("textarea");
-    smallTextInput.value = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore harum, quasi dicta ex totam quisquam quo tempore maxime, commodi praesentium eius quod suscipit! Tenetur magnam eligendi amet fugiat adipisci impedit.";
-    smallTextInput.type="text";
-    smallTextInput.className = "textInputClass";
-    smallTextInput.id = "smallTextInputId"; 
-    let parent = document.getElementById("container");
-    parent.appendChild(smallTextInput);
+//Funktion för input som returnerar elementet som matchar selector.
+function onPageLoad() {
+  let inputBox = document.querySelector("#input-number");
+
+  inputBox.oninput = function() {
+    removeAllSections(); //Returnerar ingenting.
+    makeSections(parseInt(inputBox.value)); // Parse som konverterar en string till en integer i input.value
+  };
 }
+//Ökar antalet inlägg med 1 för varje inlägg som skall skapas.
+function makeSections(count){
+  for (var i = 0; i < count; i++) { //För varje varv i loopen så öka med 1.
+    var parent = document.querySelector("main"); // returnerar element 
+    var child = document.createElement("section"); // skapar en instans för specifik element tag.
+    var title = document.createElement("h4"); // skapar en instans för specifik element tag.
+    var blurb = document.createElement("p"); // skapar en instans för specifik element tag.
 
-function removal()
-{
+    title.innerText = "Title " + i; // Ökar titel text med 1 för varje nytt inlägg.
+    blurb.innerText = `Let's creat som magic!.`; // Standard värde i rutan för inlägg.
 
+    makeEditable(title);
+    makeEditable(blurb);
+
+    child.append(title);
+    child.append(blurb);
+    parent.append(child);
+  }
+}
+//Minskar antalet inlägg med 1 som tas bort.
+function removeAllSections(){
+  var test_sections = document.querySelectorAll("section"); // returnerar alla element som matchar selektor.
+  for (var i = 0; i < test_sections.length; i++) { // returnerar värden ifrån kollektion.
+    test_sections[i].remove(); //reducerar antalet inlägg med 1
+  }
+}
+// Måste läsa på mer om hur just denna funktion fungerar, har med klick att göra så vill gärna komplettera denna.
+// Hittar dock förtillfället ingen bra information angående hur just den du har gjort fungerar.
+function makeEditable(elem){
+  elem.onclick = function(e) {
+    elem.contentEditable = true;
+    elem.focus();
+  };
+  elem.onblur = function(e) {
+    elem.contentEditable = false;
+  };
 }
