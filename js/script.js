d347af5e997759b5c15ecbaaa45252f028b479f3
Marcus Backholm <Marcus.Backholm@live.se>
2021-02-21 17:28:17 +0100

    Same Js code, few changes. comments explaining the code.

--- js/script.js
+++ js/script.js
@@ -1,48 +1,68 @@
-document.addEventListener("DOMContentLoaded", onPageLoad);
+/* Jag har använd mig utav breakpoints när jag har debuggat koden för att se vad det är som händer stegvis i koden. Vilken data/information
+som skrivsut vart och när koden exekveras. Jag har inspekterat koden under körningen genom att trycka F12 i webbläsaren. 
+Har även krivit om koden i konsolfönstret "sources" i inspektera fönstret för att även se vad som händer när vi ändrar på variabler
+och dess innehåll. Kort och gott sagt så använder jag mig utav "stepthrough" debugging i webbläsaren. 
+Har mer eller mindre ändrat om i din kod och satt tillbaka olika värden och mer förklarat vad det är som händer i koden.
+Vilka metoder det är som används och hur dom fungerar. Med tanke på att kah ligger efter så ville jag göra en så simpel komplettering som möjligt.
+Har läst på mycket teori angående DOM och funktioner och olika slags anrop i JavaScript. 
+*/
 
-//Funktion för input som returnerar elementet som matchar selector.
+
+document.addEventListener("DOMContentLoaded", onPageLoad); // Lägger till en klickhändelse i dokumentet. Döper metoden till "onPageLoad"
+
+//Funktion för inlägg.
 function onPageLoad() {
-  let inputBox = document.querySelector("#input-number");
+  let inputBox = document.querySelector("#input-number"); // returnerar det första elementet i dokumentet som marchar det angivna elementet eller en så kallad "grupp" av val. Om inga matchningatr hittas så returnerar värdet "Null"
 
+  // input funktion som kör argument anrop för dess funktioner som är skapade och körs.
   inputBox.oninput = function() {
-    removeAllSections(); //Returnerar ingenting.
-    makeSections(parseInt(inputBox.value)); // Parse som konverterar en string till en integer i input.value
+    removeAllSections(); // funktionsanrop i funktionen oninput.
+    makeSections(parseInt(inputBox.value)); //funktionsanrop makeSections. Funktionen parseInt för en analysering på ett strängargument och returnerar ett heltal
+                                            //för den angivna radixen " alltså basen" av det matematiska siffersystemet. Här kikar den efter atal inlägg som skapas.
   };
+
 }
-//Ökar antalet inlägg med 1 för varje inlägg som skall skapas.
+
+// Funktionen makeSection med en parameter som heter "count" för att loopa igenom skapande titlar med inlägg.
 function makeSections(count){
-  for (var i = 0; i < count; i++) { //För varje varv i loopen så öka med 1.
-    var parent = document.querySelector("main"); // returnerar element 
-    var child = document.createElement("section"); // skapar en instans för specifik element tag.
-    var title = document.createElement("h4"); // skapar en instans för specifik element tag.
-    var text = document.createElement("p"); // skapar en instans för specifik element tag.
+  for (var i = 0; i < count; i++) { // För varjer varav i loopen så öka antalet inlägg med 1 = i++
+    var parent = document.querySelector("main"); // variablen "parent" returnerar det första elementet i dokumentet som marchar det angivna elementet eller en så kallad "grupp" av val. Om inga matchningatr hittas så returnerar värdet "Null"
+    var child = document.createElement("section"); // variablen "child" skapar ett nytt "section" i dokumentet
+    var title = document.createElement("h4"); // variabeln "title" skapar ett nytt "h4" element i dokumentet
+    var text = document.createElement("p"); // variablen "text" skapar ett nytt "p" element i dokumentet
 
-    title.innerText = "Blogg " + i; // Ökar titel text med 1 för varje nytt inlägg.
-    text.innerText = `Type some...`; // Standard värde i rutan för inlägg.
+    title.innerText = "Blogg " + i; // Tilldelar "title.innerText" Starttextsom visar en titel som varje blogg inlägg tilldelas.
+    text.innerText = `Type some...`; // Tilldelar "text.innerText" starttext i blogginlägg rutan innan vi vill skriva något annat.
 
+
+  
     makeEditable(title);
-    makeEditable( text);
+    makeEditable(text);
 
+    // .append() infogar en uppsättning node objekt eller DOMsträngobjekt från sista child av en parent node.
     child.append(title);
     child.append(text);
     parent.append(child);
   }
 }
-//Minskar antalet inlägg med 1 som tas bort.
+
+// funktion för att ta bort antal blogginlägg som skapats.
 function removeAllSections(){
-  var test_sections = document.querySelectorAll("section"); // returnerar alla element som matchar selektor.
-  for (var i = 0; i < test_sections.length; i++) { // returnerar värden ifrån kollektion.
-    test_sections[i].remove(); //reducerar antalet inlägg med 1
+  var test_sections = document.querySelectorAll("section");
+  for (var i = 0; i < test_sections.length; i++) {
+    test_sections[i].remove(); // .remove tar gör så att vi kan ta bort det valda elemetet från dokumentet.
   }
 }
-// Måste läsa på mer om hur just denna funktion fungerar, har med klick att göra så vill gärna komplettera denna.
-// Hittar dock förtillfället ingen bra information angående hur just den du har gjort fungerar.
-function makeEditable(elem){
-  elem.onclick = function(e) {
-    elem.contentEditable = true;
-    elem.focus();
+
+// Funktion makeEditable för att editera inehåll i blogginläggen.
+// Funktionen Har en parameter "rem"
+function makeEditable(rem){
+  rem.onclick = function(e) { // .onclick aänvänds för att utlösa en funktion när ett element klickas på.
+    rem.contentEditable = true;
+    rem.focus(); // .focus sätter fokus på det angivna elementet och tar emot användar input som standard.
   };
-  elem.onblur = function(e) {
-    elem.contentEditable = false;
+  //funktion
+  rem.onblur = function(e) { // .onblur kör ett script när användaren lämnar ett inmatningsfält.
+    rem.contentEditable = false;
   };
 }
